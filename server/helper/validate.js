8afbda4f48518983dc20cd39280a04d3c6a22aa4
lemankay <46055728+lemankay@users.noreply.github.com>
2021-06-08 12:21:32 +0300

    Update validate.js

--- server/helper/validate.js
+++ server/helper/validate.js
@@ -1,35 +1,12 @@
 module.exports.validateLogin = (username, password) => {
-    const errors = {}
-    if(username.trim() === "") {
-        errors.username = "Введите имя пользователя,поле не может быть пустым"
-    }
-    if(password.trim() === "") {
-        errors.password = "Введите пвраль пользователя он не может быть пустым"
-    }
-    return {
-        errors,
+
         validate: Object.keys(errors).length > 0
     }
 }
 
 module.exports.validateRegister = (email,password,confirmPassword,username) => {
     const errors = {}
-    if(email.trim() === "") {
-        errors.email = "Введите email пользователя"
-    } else {
-        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-        if(!email.match(regEx)) {
-             errors.email = "Пожалуйста Введите коректный Email ";
-        }
-    }
-    if(username.trim() === "") {
-        errors.username = "Имя пользователя не может быть пустым,Введите имя пользователя"
-    }
-    if(password.trim() === "") {
-        errors.password = "Пароль не может быть пустым,Введите пвраль пользователя"
-    }else  if(confirmPassword.trim() !== password) {
-        errors.confirmPassword = "Пвраль не совпадает, Введи правельный пароль"
-      }
+
     return {
         errors,
         validate: Object.keys(errors).length > 0
