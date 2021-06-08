ed97852e331ad7173a550732842da357920dc150
lemankay <46055728+lemankay@users.noreply.github.com>
2021-06-08 12:21:01 +0300

    Update auth.js

--- server/helper/auth.js
+++ server/helper/auth.js
@@ -3,16 +3,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.auth = (context) => {
    //context = { ...headers }
-  const authHeader = context.req.headers.authorization;
-  if (authHeader) {
-    // Bearer ....
-    const token = authHeader.split('Bearer ')[1];
-    if (token) {
-      try {
-        const user = jwt.verify(token , "SOME PRIVAT KEY");
-        return user;
-      } catch (err) {
-        throw new AuthenticationError('Invalid/Expired token');
+xpired token');
       }
     }else{
        throw new Error('Вы не залогинены');
